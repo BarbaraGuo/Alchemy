c41152a00274c6db005bb1af47c51535192008d5
RAsadi <asadi.rayan@gmail.com>
2020-03-26 19:55:22 -0400

    Adding character controller with .gitignore Removed unnecessarily commited files

---
+++ Assets/Scripts/MovingPlatform.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovingPlatform : MonoBehaviour {
+    public Transform start;
+    public Transform end;
+    [SerializeField] private float speed = 2f;
+
+    Vector3 nextPos;
+    // Start is called before the first frame update
+    void Start() {
+        nextPos = start.position;
+    }
+
+    // Update is called once per frame
+    void Update() {
+        if (transform.position == start.position) {
+            nextPos = end.position;
+        } else if (transform.position == end.position) {
+            nextPos = start.position;
+        }
+        transform.position = Vector3.MoveTowards(transform.position, nextPos, speed * Time.deltaTime);
+    }
+
+    private void OnDrawGizmos() {
+        Gizmos.DrawLine(start.position, end.position);
+    }
+}
--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -6,66 +6,124 @@ using System;
 public class PlayerMovement : MonoBehaviour //This class name must match that in the asset, no automatic refactoring
 {
 
-    [SerializeField] float      m_speed = 5.0f;
-    [SerializeField] float      m_jumpForce = 2.0f;
+    [Header("Horizontal Movement")]
+    [SerializeField] private float moveSpeed = 14f;
+    [SerializeField] private Vector2 direction;
+    [SerializeField] private float thresholdVelocity = 3f;
+    [SerializeField] private float thresholdDragFactor = 4f;
+    private bool facingLeft = true;
 
-    private Vector2 m_Movement; 
-    private Animator m_animator;
-    private Rigidbody2D m_body2d;
-    private Sensor_Bandit m_groundSensor;
-    private bool m_grounded;
+    [Header("Vertical Movement")]
+    [SerializeField] private float jumpForce = 9f;
+    [SerializeField] private float jumpDelay = 0.25f;
+    private float jumpTimer = 0f;
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        m_grounded = false;
-        m_animator = GetComponent<Animator>();
-        m_body2d = GetComponent<Rigidbody2D>();
-        m_groundSensor = transform.Find("GroundSensor").GetComponent<Sensor_Bandit>();
-    }
+    [Header("Collision")]
+    public bool isGrounded = false; // Public because GroundCheck needs to deal with this
+    [SerializeField] private float distanceToGround = 0.01f;
+
+    [Header("Components")]
+    public Rigidbody2D rb;
+    public Animator animator;
+    public LayerMask groundLayer;
+
+    [Header("Physics")]
+    [SerializeField] private float maxSpeed = 7f;
+    [SerializeField] private float linearDrag = 5f;
+    [SerializeField] private float gravity = 1.4f;
+    [SerializeField] private float fallMultiplier = 4f;
+    [SerializeField] private float linearDragJumpScale = 0.15f;
 
     // Update is called once per frame
-    void Update() 
-    {
-        //Check if character just landed on the ground
-        if (!m_grounded && m_groundSensor.State()) {
-            m_grounded = true;
-            m_animator.SetBool("Grounded", m_grounded);
+    void Update() {
+        // Raycast to floor to see if we are grounded, only needs to be called once per frame
+        // TODO: change this to a boxcast
+        //isGrounded = Physics2D.Raycast(transform.position, Vector2.down, distanceToGround);
+
+        direction = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
+
+        if (Input.GetKeyDown(KeyCode.W) || Input.GetKeyDown(KeyCode.UpArrow)) {
+            jumpTimer = Time.time + jumpDelay;
         }
 
-        //Check if character just started falling
-        if(m_grounded && !m_groundSensor.State()) {
-            m_grounded = false;
-            m_animator.SetBool("Grounded", m_grounded);
-        }
-
-        // Use Unity's input manager 
-        // Horizontal = a,d,left,right keys
-        float horizontal = Input.GetAxis("Horizontal");
-        float vertical = Input.GetAxis("Vertical");
-
-        bool isWalking = !Mathf.Approximately(horizontal,0f);
-        m_animator.SetBool("IsWalking", isWalking);
-
-        if(m_grounded){
-            m_body2d.velocity = new Vector2(horizontal * m_speed, m_body2d.velocity.y);
-        }
-    
-        m_animator.SetFloat("AirSpeed", m_body2d.velocity.y);
-
-        if ( horizontal > 0)
-            GetComponent<SpriteRenderer>().flipX = true;
-        else if (horizontal < 0)
-            GetComponent<SpriteRenderer>().flipX = false;
-
-        // Handle movement
-        if ((Input.GetKeyDown("space") || Input.GetKeyDown("up")) && m_grounded) {
-            m_grounded = false;
-            m_animator.SetBool("Grounded", m_grounded);
-            m_body2d.velocity = new Vector2(m_body2d.velocity.x, m_jumpForce);
-            m_groundSensor.Disable(0.2f);
-        }
-
+        HandleAnimation(direction.x);
     }
-    
+
+    // FixedUpdate is better for physics because can be called
+    // multiple times per frame
+    void FixedUpdate() {
+        Move(direction.x);
+        ApplyGravity();
+        ApplyDrag();
+
+        // delay time to allow for multiple frames of jumpp
+        if (jumpTimer > Time.time && isGrounded) {
+            Jump();
+        }
+    }
+
+    void Move(float horizontal) {
+        // actual movement
+        rb.AddForce(Vector2.right * horizontal * moveSpeed);
+
+        // max vel
+        if (Mathf.Abs(rb.velocity.x) > maxSpeed) {
+            rb.velocity = new Vector2(Math.Sign(rb.velocity.x) * maxSpeed, rb.velocity.y);
+        }
+    }
+
+    // Drag is used for slowing down after letting go of the run button
+    void ApplyDrag() {
+        bool isChangingDir = (direction.x > 0 && rb.velocity.x < 0) || (direction.x < 0 && rb.velocity.x > 0);
+        if (isGrounded) {
+            if (Mathf.Abs(direction.x) < 0.4f || isChangingDir) {
+                if (Math.Abs(rb.velocity.x) < thresholdVelocity) {
+                    rb.drag = linearDrag * thresholdDragFactor;
+                } else {
+                    rb.drag = linearDrag;
+                }
+            } else {
+                rb.drag = 0;
+            }
+        }
+    }
+
+    void ApplyGravity() {
+        // See "Better Jumping in Unity with Four Lines of Code"
+        // For most of the garbage
+        // https://www.youtube.com/watch?v=7KiK0Aqtmzc 
+        if (!isGrounded) {
+            rb.gravityScale = gravity;
+            rb.drag = linearDrag * linearDragJumpScale;
+
+            if (rb.velocity.y < 0) {
+                rb.gravityScale = gravity * fallMultiplier;
+            } else if (rb.velocity.y > 0 && !(Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.UpArrow))) {
+                rb.gravityScale = gravity * fallMultiplier * 0.5f;
+            }
+
+        } else {
+            rb.gravityScale = 0;
+        }
+    }
+
+    // Helper for changing the direction the player is lookin
+    void Flip() {
+        facingLeft = !facingLeft;
+        transform.rotation = Quaternion.Euler(0, facingLeft ? 0 : 180, 0);
+    }
+
+    void Jump() {
+        // reset velocity then jump
+        rb.velocity = new Vector2(rb.velocity.x, 0);
+        rb.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
+    }
+
+    void HandleAnimation(float horizontal) {
+        animator.SetBool("IsGrounded", isGrounded);
+        animator.SetFloat("Horizontal", Math.Abs(direction.x));
+        if (horizontal < 0 && !facingLeft || horizontal > 0 && facingLeft) {
+            Flip();
+        }            
+    }
 }
